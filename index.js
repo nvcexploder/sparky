4b097a4a83b68930c6987c7a5be26982b5f2a6d3
Ben Acker <benacker@gmail.com>
2020-09-16 23:31:09 -0400

    Add fastify

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const conf = {
-  token: process.env['TOKEN']
+  token: process.env['TOKEN'],
+  port: process.env['PORT']
 }
+const fastify = require('fastify')({
+  logger: true
+})
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -16,3 +20,8 @@ client.on('message', msg => {
 });
 
 client.login(conf.token);
+
+fastify.listen(conf.port, (err, address) => {
+  if (err) throw err
+  fastify.log.info(`server listening on ${address}`)
+})
