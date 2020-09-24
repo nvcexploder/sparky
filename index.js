498e9f75955eefde36639343bdd06d50c72a6568
Ben Acker <benacker@gmail.com>
2020-09-23 22:00:10 -0400

    ?

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const conf = require('rc')('sparky');
-const TTS=require("discord-tts");
+const TTS = require("discord-tts");
 
 // commands - these files should be named after the commands the perform
 // for example, !roll -> roll
@@ -22,10 +22,11 @@ client.on('message', msg => {
   if (msg.content.indexOf(`!justa20`)===0) {
     msg.reply(`is a cheater`)
   }
-  if(msg.content==="!ssay"){
+  if(msg.content.indexOf(`!say`) > -1){
+    let utterance = msg.content.substring(5, msg.content.length()-1)
     const voiceChannel = msg.member.voice.channel;
     voiceChannel.join().then(connection => {
-        const stream = discordTTS.getVoiceStream("sweaty gams");
+        const stream = TTS.getVoiceStream(utterance);
         const dispatcher = connection.play(stream);
         dispatcher.on("finish",()=>voiceChannel.leave())
     });
